ad9a1b5df0187d90e096e404e1c27a31b60671aa
mahdi7723 <parvizimahdi94@gmail.com>
2023-07-21 23:44:17 +0330

    create onlinebook project

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password'),
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'avatar', 'phone', 'address'),
+        }),
+        # ... other sections ...
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
